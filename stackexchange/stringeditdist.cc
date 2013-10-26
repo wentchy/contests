a106f0d37414e1853135de4e50127e3c9c34b5b7
wentchy <liukai@liukai-t400.(none)>
2013-10-26 16:05:09 +0100

    reduce memory size.

--- stackexchange/stringeditdist.cc
+++ stackexchange/stringeditdist.cc
@@ -4,36 +4,42 @@
 
 const int dp_size = 100;
 
-int dp[dp_size + 1][dp_size + 1];
+//int dp[dp_size + 1][dp_size + 1];
+int dp1[dp_size + 1];
+int dp2[dp_size + 1];
 
 int edit_dist(const char* src, const char* dst)
 {
     int srclen = std::strlen(src);
     int dstlen = std::strlen(dst);
+    int* last_dp = dp1;
+    int* current_dp = dp2;
     for (int i = 1; i < srclen + 1; ++i)
     {
+        current_dp[0] = i;
         for (int j = 1; j < dstlen + 1; ++j)
         {
-            int dist = dp[i - 1][j - 1];
+            int dist = last_dp[j - 1];
             if (src[i - 1] != dst[j - 1])
             {
                 dist += 2;
             }
-            dist = std::min(dist, dp[i - 1][j] + 1);
-            dist = std::min(dist, dp[i][j - 1] + 1);
-            dp[i][j] = dist;
+            dist = std::min(dist, last_dp[j] + 1);
+            dist = std::min(dist, current_dp[j - 1] + 1);
+            current_dp[j] = dist;
         }
+        std::swap(last_dp, current_dp);
     }
-    return dp[srclen][dstlen];
+    return last_dp[dstlen];
 }
 
 void init_dp()
 {
-    std::memset(dp, 0, sizeof(dp));
+    std::memset(dp1, 0, sizeof(dp1));
+    std::memset(dp2, 0, sizeof(dp2));
     for (int i = 0; i < dp_size + 1; ++i)
     {
-        dp[i][0] = i;
-        dp[0][i] = i;
+        dp1[i] = i;
     }
 
 }
